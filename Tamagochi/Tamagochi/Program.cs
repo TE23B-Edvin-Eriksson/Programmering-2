acdbdc2fb7ae2ab3ee039762a6662066a8fc1766
Edvin Eriksson <edvin.eriksson@elev.ga.ntig.se>
2025-09-26 13:17:38 +0200

    Klar

--- Tamagochi/Tamagochi/Program.cs
+++ Tamagochi/Tamagochi/Program.cs
@@ -1,1 +1,52 @@
-﻿
+using System.Formats.Asn1;
+
+Console.WriteLine("Welcome to Tamagotchi");
+
+Tamagotchi tama = new();
+tama.Start();
+
+Console.WriteLine("Write the name of your Pet");
+tama.name = Console.ReadLine();
+
+while (tama.GetAlive() == true)
+{
+    Console.Clear();
+    tama.Printstats();
+    Console.WriteLine($"What do you want to do?");
+    Console.WriteLine($"1. Feed {tama.name}");
+    Console.WriteLine($"2. Play with {tama.name}");
+    Console.WriteLine($"3. Teach {tama.name} a word");
+    Console.WriteLine($"4.PrintStats");
+    Console.WriteLine($"5. Do Nothing");
+
+    string answer = Console.ReadLine();
+
+    if (answer == "1")
+    {
+        tama.Feed();
+    }
+    else if (answer == "2")
+    {
+        tama.Play();
+    }
+    else if (answer == "3")
+    {
+        tama.Teach();
+    }
+    else if (answer == "4")
+    {
+        tama.Printstats();
+    }
+    else if (answer == "5")
+    {
+
+    }
+    else
+    {
+
+    }
+    tama.Tick();
+
+}
+
+Console.ReadLine();
---
+++ Tamagochi/Tamagochi/Tamagochi.cs
@@ -0,0 +1,69 @@
+﻿public class Tamagotchi
+{
+    private int hunger = 10;
+    private int boredom = 10;
+    private List<string> words = [];
+    private List<string> Games = ["You play Fetch with", "You play cards with", "You start dancing with",];
+    private List<string> Food = ["You give chicken nuggets to", "You give Pineapple to", "You give a hamburger to",];
+    public string name = "";
+
+    public void Start()
+    {
+        GetAlive();
+    }
+
+    public void Tick()
+    {
+        hunger--;
+        boredom++;
+    }
+
+    public void Feed()
+    {
+        hunger++;
+        int r = Random.Shared.Next(0, 3);
+        Console.WriteLine($"{Food[r]}" + $"{name}");
+        Thread.Sleep(2000);
+    }
+
+    public void Play()
+    {
+        boredom--;
+        int r = Random.Shared.Next(0, 3);
+        Console.WriteLine($"{Games[r]}" + $"{name}");
+        Thread.Sleep(2000);
+    }
+
+
+    public void Teach()
+    {
+        Console.WriteLine($"What word do you want to teach {name}");
+        words.Add(Console.ReadLine());
+        Thread.Sleep(1000);
+        Console.WriteLine($"You have taught {name} {words.Last()}!");
+        Thread.Sleep(1000);
+    }
+
+    public void Printstats()
+    {
+        Console.WriteLine($"Boredom: {boredom}, hunger:{hunger}");
+        Thread.Sleep(1000);
+    }
+
+    public bool GetAlive()
+    {
+        return IsAlive();
+    }
+
+    public bool IsAlive()
+    {
+        if (hunger < 0 || boredom < 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
